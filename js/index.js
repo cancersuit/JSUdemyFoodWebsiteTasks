c64328cc92801b15f00240afb63591c72e3dc35f
Ilya Malofeev <cancersuit@mail.ru>
2023-04-03 15:55:12 +0300

    second task food website

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+//задание с выбором стиля питания
+
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
     const tabsParent = document.querySelector('.tabheader__items')
@@ -34,6 +36,64 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     })
-})
 
-console.log('asdasdasd')
+
+    //задание с таймером
+
+    const deadline = '2023-03-10';
+
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date()); //принимается наш дедлайн и из него вычитается нынешняя дата
+        if(t <= 0) {
+            days = hours = minutes = seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)), // наше кол-во милисекунд t делим на количество мс в одном дне
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24), //после деления получим кол-во часов, которое может содержаться в сутках
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
+
+              return { //возвращаем объект со всеми значениями
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        }
+
+        function getZero(num) { //функция проверки числа, если оно меньше 10 - в начало подставить 0 (для даты это может быть важно)
+            if (num >=0 && num < 10) {
+                return `0${num}`;
+            } else {
+                return num;
+            }
+        }
+
+        function setClock(selector, endtime) {
+            const timer = document.querySelector(selector),
+                  days = timer.querySelector('#days'),
+                  hours = timer.querySelector('#hours'),
+                  minutes = timer.querySelector('#minutes'),
+                  seconds = timer.querySelector('#seconds'),
+                  timeInterval = setInterval(updateClock, 1000);
+
+            updateClock(); //вызов функции обновления времени, так как без нее странице понадобится секунда, чтобы подгрузить актуальные значения таймера
+
+            function updateClock() {
+                const t = getTimeRemaining(endtime);
+
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                }
+            }
+        }
+
+        setClock('.timer', deadline);
+})
